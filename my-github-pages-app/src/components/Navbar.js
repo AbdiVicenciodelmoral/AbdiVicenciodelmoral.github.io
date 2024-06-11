db9c141a1077250b7c046b69dbe83e077e57311e
AbdiVicenciodelmoral <a.vicenciodelmoral@gmail.com>
2024-06-11 11:37:57 -0700

    Added Navbar component and updated site structure

--- my-github-pages-app/src/components/Navbar.js
+++ my-github-pages-app/src/components/Navbar.js
@@ -1,9 +1,21 @@
 import './Navbar.css'
 
+/*The <ul> element is for grouping a collection of items that do not have a numerical ordering, 
+and their order in the list is meaningless. Typically, unordered-list items are displayed with 
+a bullet, which can be of several forms, like a dot, a circle, or a square. The bullet style is 
+not defined in the HTML description of the page, but in its associated CSS, using the list-style-type property.
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/ul
+*/
+
+
+
 const Navbar = () => {
     return(
         <nav className="navbar">
             <h1>Abdi Vicenciodelmoral</h1>
+            <ul>
+                <li><a href="#home">Home</a></li>
+            </ul>
         </nav>
     )
     
